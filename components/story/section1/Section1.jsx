ce2892f3eb9c53cc800cf04249d823a57d2b4c80
Adam Rifsal <adam.finnest@gmail.com>
2026-07-21 00:23:59 +0530

    refactor: remove textVh prop from Section1 and update manifest timestamp

--- components/story/section1/Section1.jsx
+++ components/story/section1/Section1.jsx
@@ -15,7 +15,7 @@ import { SECTION1_ID, SECTION1_TEXTS } from "@/data/story/section1";
  */
 export const Section1 = memo(function Section1() {
   return (
-    <StorySection id={SECTION1_ID} texts={SECTION1_TEXTS} animationVh={600} textVh={1200}>
+    <StorySection id={SECTION1_ID} texts={SECTION1_TEXTS} animationVh={600}>
       {({ progress }) => <Section1Scene progress={progress} />}
     </StorySection>
   );
